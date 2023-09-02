899698f5dc8af36c04acee245370755d30361878
Nacho-93 <ignacioindurainusa@hotmail.com>
2023-09-02 18:22:48 -0300

    c

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+import NavBar from "./components/NavBar";
+
+export default function App() {
+  return (
+    <div className="app--container">
+      <NavBar />
+      
+    </div>
+  )
+}
