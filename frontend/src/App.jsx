4a8d20eb4903082a4d1298cff1d067fdbff647ba
Nacho-93 <ignacioindurainusa@hotmail.com>
2023-09-09 03:10:31 -0300

    c

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <div className="app--container">
       <NavBar />
-      
     </div>
   )
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import logo from "../assets/descarga.png"
+import logo from "../assets/logo.png"
+import "./NavBar.css"
 export default function NavBar() {
 
   return (
-    <nav class="navbar navbar-expand-sm navbar-secondary bg-secondary">
+    <nav class="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "rgba(20, 20, 20, 0.8)"}}>
       <div className="container">
-        <a href="#" className="navbar-brand mb-0 h1">
+        <a href="##" className="navbar-brand mb-0 h1">
           <img src={logo} alt="Logo"
             className="d-inline-block align-top"
-            width="30" height="30" /> NavBar
+            width="150" height="60" />
         </a>
 
         <button
@@ -23,30 +24,38 @@ export default function NavBar() {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a href="#" className="nav-link">
-                Inicio
-              </a>
-            </li>
+          <ul className="navbar-nav mx-auto text-center p-10">
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Servicios
+              <a class="nav-link dropdown-toggle" href="##" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Categorias
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="##">Física</a></li>
+                <li><a class="dropdown-item" href="##">Inglés</a></li>
+                <li><a class="dropdown-item" href="##">Matemática</a></li>
+                <li><a class="dropdown-item" href="##">Programación</a></li>
+                <li><a class="dropdown-item" href="##">Portugués</a></li>
+                <li><a class="dropdown-item" href="##">Química</a></li>
               </ul>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link">
+              <a href="##" className="nav-link">
                 Sobre nosotros
               </a>
             </li>
+            <li className="nav-item">
+              <a href="##" className="nav-link">
+                Contacto
+              </a>
+            </li>
           </ul>
+  
+          <button className="btn btn-outline-light d-sm-inline-block m-2">Iniciar sesión</button>
+          <button className="btn btn-outline-primary d-sm-inline-block m-2">Registrarse</button>
+    
         </div>
+   
+        
       </div>
     </nav>
   )
