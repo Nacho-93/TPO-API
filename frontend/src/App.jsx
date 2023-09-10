be86e0bd9929f9a9dbb1908112e1ab2eade82003
Nacho-93 <ignacioindurainusa@hotmail.com>
2023-09-10 01:25:20 -0300

    c

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import react from "react";
 import NavBar from "./components/NavBar";
+import "./App.css";
+
 
 export default function App() {
   return (
     <div className="app--container">
       <NavBar />
+      <body>
+        
+      </body>
     </div>
   )
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,62 +1,79 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import logo from "../assets/logo.png"
 import "./NavBar.css"
 export default function NavBar() {
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "rgba(20, 20, 20, 0.8)"}}>
-      <div className="container">
-        <a href="##" className="navbar-brand mb-0 h1">
+    <nav class="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:"rgba(30,30,30, 0.5)"}}>
+  <div class="container">
+
+    {/* LOGO */}
+    <a href="##" className="navbar-brand mb-0 h1">
           <img src={logo} alt="Logo"
             className="d-inline-block align-top"
             width="150" height="60" />
         </a>
 
-        <button
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          className="navbar-toggler">
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mx-auto text-center p-10">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="##" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Categorias
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="##">Física</a></li>
-                <li><a class="dropdown-item" href="##">Inglés</a></li>
-                <li><a class="dropdown-item" href="##">Matemática</a></li>
-                <li><a class="dropdown-item" href="##">Programación</a></li>
-                <li><a class="dropdown-item" href="##">Portugués</a></li>
-                <li><a class="dropdown-item" href="##">Química</a></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a href="##" className="nav-link">
-                Sobre nosotros
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="##" className="nav-link">
-                Contacto
-              </a>
-            </li>
-          </ul>
-  
-          <button className="btn btn-outline-light d-sm-inline-block m-2">Iniciar sesión</button>
-          <button className="btn btn-outline-primary d-sm-inline-block m-2">Registrarse</button>
+    {/* TOGGLE BUTTON */}
+    <button 
+    class="navbar-toggler shadow-none border-0" 
+    type="button" 
+    data-bs-toggle="offcanvas" 
+    data-bs-target="#offcanvasNavbar" 
+    aria-controls="offcanvasNavbar" 
+    aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     
-        </div>
-   
-        
+    {/* SIDEBAR */}
+    <div class="sidebar offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header text-white border-bottom">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close btn-close-white shadow-none border-0" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-    </nav>
-  )
-}
+
+      <div class="offcanvas-body">
+
+        {/* NavITEMS */}
+        <ul class="navbar-nav justify-content-center fs-5 flex-grow-1 pe-0">
+          <li class="nav-item mx-2">
+            <a class="nav-link active" aria-current="page" href="#inicio">Inicio</a>
+          </li>
+          <li class="nav-item dropdown mx-2">
+            <a class="nav-link dropdown-toggle" href="#categorias"
+             role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categorias
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#fisica">Física</a></li>
+                <li><a class="dropdown-item" href="#ingles">Inglés</a></li>
+                <li><a class="dropdown-item" href="#matematica">Matemática</a></li>
+                <li><a class="dropdown-item" href="#programacion">Programación</a></li>
+                <li><a class="dropdown-item" href="#portugues">Portugués</a></li>
+                <li><a class="dropdown-item" href="#quimica">Química</a></li>
+            </ul>
+        </li>
+
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#nosotros">Sobre nosotros</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#contacto">Contacto</a>
+          </li>
+
+        </ul>
+          {/* Login / Sign Up */}
+          <div className="div-login d-flex align-items-start pt-2">
+          <button className="login log btn btn-link text-white">Iniciar sesión</button>
+          <button className="login register btn btn-outline-info text-white"
+          style={{border: "1.5px solid #006668"}}>Registrarse</button>
+          </div>
+          
+      </div>
+    </div>
+  </div>
+</nav>
+);
+  }
