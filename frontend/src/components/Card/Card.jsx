541f87f483bd86cdb7b4d754be41939a90fcd24e
Nacho-93 <ignacioindurainusa@hotmail.com>
2023-09-23 03:13:50 -0300

    Card para pc

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -4,32 +4,45 @@ import pict from "../../assets/tutor1.jpg"
 export default function Card(props) {
     return (
 
-        <div class="card mb-2" style={{ maxWidth: "540px" }}>
-            <div class="row g-0">
-                <div className="row g-0">
-                    <div className="col-md-2">
-                        <img src={pict}
-                            height="90px"
-                            width="90px" class="card-image img-fluid rounded-start rounded" alt="..." />
-                        <div className="name-rating-price col-md-10">
-                            <p class="text-body">John Johnson</p>
-                        </div>
+        <div className="card">
+            <div className="card-body row profesor-content">
+                <div className="col-md-1 g-0">
+                    <div className="profesor-img">
+                        <span className="round-photo">
+                            <img src={pict} alt="profesor" className="img-fluid" />
+                        </span>
+                    </div>
+                </div>
+                <div className="col-md-7">
+                    <div className="profesor-course">
+                        <a href="#profile" className="card-title name">John Johnson</a>
+                        <h3 className="card-title clase">Clase de Algrebra</h3>
+                        <p className="card-text text-muted profesor-description">Hello! I'm a dedicated and experienced programming tutor here to help you master the art of coding. I possess a deep understanding of various programming languages and paradigms. My goal is to empower students like you with the knowledge and skills necessary to excel in the world of programming.<a href="#profile" className="text-muted profesor-profile">...ver perfil</a>
+                        </p>
+
                     </div>
                 </div>
 
-                <div class="col-md-9">
-                    <div class="card-body">
+                <div className="col-md-1 line"></div>
 
-                        <h5 class="card-title">Analisis Matematico I</h5>
-                        <p class="card-text">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo delectus cum id nulla facere consectetur, deserunt temporibus molestiae corporis qui quaerat dicta laborum adipisci ipsum non odio unde et amet!</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                <div className="col-md-3">
+                    <div className="profesor-stats">
+                        <h6 className="card-title"><i>$</i> 50/hora</h6>
+                        <h6 className="card-text text-muted"><i class="fa-solid fa-hourglass-end"></i> 54 horas</h6>
+                        <h6 className="card-title">Presencial/Online</h6>
+                        <h6 className="card-title">Individual/Grupal</h6>
+                        <button className="btn btn-primary">Contratar</button>
                     </div>
                 </div>
             </div>
+            <div className="card-body row profesor-lastrow">
+                <div className="col-md-10 comment">
+                    <p className="card-text"><i class="fa-solid fa-quote-left"></i>  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, aliquid architecto est numquam omnis cumque sunt maiores velit atque quae rem, rerum repellat accusantium. Architecto, unde saepe eligendi sit quaerat quisquam! Dolor.</p>
+                </div>
+                <div className="col-md-2 reviews">
+                    <button className="btn btn-outline-secondary">Opiniones</button>
+                </div>
+            </div>
         </div>
-
-
-
     )
 }
