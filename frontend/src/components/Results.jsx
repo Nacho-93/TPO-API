3098becccb885db31457f20cf641d39eeac06e1e
Nacho-93 <ignacioindurainusa@hotmail.com>
2023-10-04 00:10:23 -0300

    Update Data y Card

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -18,21 +18,25 @@ function Results() {
         return (
             professor.courses.map((course) => {
                 let last_review = null;
+                let rating_amount = false;
                 if (course.reviews.length > 0) {
                     last_review = course.reviews[course.reviews.length - 1].comment;
+                    rating_amount = [course.reviews.reduce((sum, review) => sum + review.rating, 0) / course.reviews.length, course.reviews.length]
                 }
                 return (
                     <Card
                         name={professor.name}
                         lastName={professor.lastName}
-                        description={professor.description}
+                        // description={professor.description}   CAMBIAMOS POR course_description
                         image_profile={professor.image_profile}
-                        hours_experience={professor.hours_experience}
+                        frequency={course.frequency}                 // [cantidad, unidad] ej: [1, "semana"]
                         id={professor.id}
-                        title={course.title}
+                        title={course.title}                            // "fisica"
                         price_hour={course.price_hour}
+                        course_description={course.course_description}
                         last_review={last_review ? last_review : false}
-                        info={course.info_course}
+                        info={course.info_course}                       // [individual, group, in_person, online] BOOLEAN
+                        rating_amount={rating_amount}
                     />
                 )
             }
